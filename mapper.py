b21c3e689d29c15e6286980893a2232145120005
tlqnguyen <trang.ng121@gmail.com>
2020-06-04 08:51:12 +0200

    Change the mapper so that an error is raised

--- mapper.py
+++ mapper.py
@@ -36,9 +36,11 @@ for line in sys.stdin:
 
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
+    if len(data) < 6:
+	print('There are not 6 elements in the tuple')
 
     # Write the key-value combination to standard output (stdout)
     # Key is the payment, value is the sales     
     # With a tab (\t) between key and value
-    # New line \n means new record
+    # New line \n means new record 
     sys.stdout.write("{0}\t{1}\n".format(category, sales))
